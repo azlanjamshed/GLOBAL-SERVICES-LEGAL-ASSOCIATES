341476f985dc5935a652da14d0c712b023a982b5
Azlan Jamshed <152954543+azlanjamshed@users.noreply.github.com>
2025-09-08 22:04:27 +0530

    edit  home button

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -5,42 +5,11 @@ import { NavLink } from "react-router-dom";
 
 const Services = () => {
   return (
-    // <div className="relative">
-    //   <span className="sticky top-[95px] right-4 bg-[#272626] text-lg font-semibold text-white px-4 py-2 z-10">
-    //     Home
-    //   </span>
-    //   <div className="w-full bg-[#272626] py-16 px-6 md:px-20">
-    //     {/* <h2 className="text-3xl md:text-4xl font-bold text-center text-gray-800 mb-12">
-    //     All Services
-    //   </h2> */}
-
-    //     <div className="text-3xl md:text-4xl font-bold text-center text-gray-800 mb-12">
-    //       <Title text1={"All"} text2={"Services"} />
-    //     </div>
-
-    //     <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
-    //       {services.map((service, index) => (
-    //         <div
-    //           key={index}
-    //           className="bg-gray-300 p-6 rounded-xl shadow-md hover:shadow-3xl hover:shadow-amber-300 transition-all duration-300 text-center flex flex-col items-center"
-    //         >
-    //           {/* {service.icon} */}
-    //           <h3 className="mt-4 text-xl font-semibold text-gray-800">
-    //             {service.title}
-    //           </h3>
-    //           <p className="mt-2 text-gray-600 text-sm">
-    //             {service.description}
-    //           </p>
-    //         </div>
-    //       ))}
-    //     </div>
-    //   </div>
-    // </div>
     <div className="relative">
       {/* Breadcrumb / Back button */}
       <NavLink
         to={"/"}
-        className="fixed top-25 left-0 bg-amber-300 hover:bg-amber-400  text-sm md:text-base font-semibold text-gray-800 px-6 py-3 block z-10 shadow-md"
+        className="fixed top-25 left-0 bg-amber-300 hover:bg-amber-400  text-sm md:text-base font-semibold text-gray-800 px-6 py-3 block z-10 shadow-md rounded-tr-xl rounded-br-xl"
       >
         {/* <span>Home</span> */}
         Home
