62666b7ad33e956373f0b26ffa68b51e536ef17f
Azlan Jamshed <152954543+azlanjamshed@users.noreply.github.com>
2025-09-16 13:06:31 +0530

    edit pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,11 @@
 import Hero from "../components/Hero";
 import Services from "./Services";
-import HomeServices from "../components/HomeService";
 
 const Home = () => {
   return (
     <>
       <Hero />
-      
+
       <Services />
     </>
   );
