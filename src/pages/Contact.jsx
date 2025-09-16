8954449f44fc654e9c2090f282bd5f30062c4aca
Azlan Jamshed <152954543+azlanjamshed@users.noreply.github.com>
2025-09-16 12:55:53 +0530

    change email

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,20 +5,6 @@ import { NavLink } from "react-router-dom";
 
 const Contact = () => {
   return (
-    // <div className="relative w-full px-4 sm:px-6 lg:px-12 py-10 bg-gray-50">
-    //   <NavLink
-    //     to={"/"}
-    //     className="fixed top-25 left-0 bg-amber-300 hover:bg-amber-400  text-sm md:text-base font-semibold text-gray-800 px-6 py-3 block z-10 shadow-md rounded-tr-xl rounded-br-xl"
-    //   >
-    //     {/* <span>Home</span> */}
-    //     Home
-    //   </NavLink>
-    //   <div className="flex  flex-col-reverse lg:flex-row gap-8 md:justify-around  ">
-    //     <Map />
-    //     <ContactForm />
-    //   </div>
-    // </div>
-
     <div className="relative w-full px-4 sm:px-6 lg:px-12 py-10 bg-[#272626]">
       <NavLink
         to={"/"}
